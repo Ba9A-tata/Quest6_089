04f9c0a24003f9e742157c8ee80dc56905be9cf8
Ba9A-tata <yogadzakkii@gmail.com>
2025-11-20 12:05:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/navigasidata/view/uiController/Navigasi.kt
@@ -0,0 +1,58 @@
+package com.example.navigasidata.view.uiController
+
+
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasidata.view.FormIsian
+import com.example.navigasidata.view.TampilData
+
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DateApp(
+    navController: NavHostController = rememberNavController(),
+    modifier : Modifier
+){
+    Scaffold{ isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian (
+                    onSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulirku(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+) {
+    navController.popBackStack(Navigasi.Formulirku.name,
+        inclusive = false)
+}
